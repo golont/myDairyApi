ed6e04bef6b0164a99a82bf287535e8721ec95ba
golont <vladpanda123@gmail.com>
2019-08-28 13:14:47 +0300

    time/duration

--- app/controllers/timer.controller.js
+++ app/controllers/timer.controller.js
@@ -2,9 +2,10 @@ const moment = require("moment");
 
 exports.getRemainTime = (req, res) => {
     const now = moment();
-    // const now = moment("2019-08-28T23:51:54.999");
     const endOfDay = moment().endOf("day");
     const duration = moment.duration(endOfDay.diff(now));
-    const remain = duration.hours() + ":" + duration.minutes() + ":" + duration.seconds();
-    res.send({ time: remain });
+    res.send({
+        time: moment.utc(duration.as("milliseconds")).format("HH:mm:ss"),
+        duration: duration
+    });
 };
