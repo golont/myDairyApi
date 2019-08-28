71efec53bd7fc74e921ccc272016f20f059a98c7
golont <vladpanda123@gmail.com>
2019-08-28 14:00:03 +0300

    timer done / changed duration to date

--- app/controllers/timer.controller.js
+++ app/controllers/timer.controller.js
@@ -5,7 +5,7 @@ exports.getRemainTime = (req, res) => {
     const endOfDay = moment().endOf("day");
     const duration = moment.duration(endOfDay.diff(now));
     res.send({
+        date: moment().format("YYYY-MM-DD"),
         time: moment.utc(duration.as("milliseconds")).format("HH:mm:ss"),
-        duration: duration
     });
 };
